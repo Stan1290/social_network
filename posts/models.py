85f3f77675214871f83eb5f1b9859ae00c80939e
Stan1290 <31145697+Stan1290@users.noreply.github.com>
2017-12-17 16:02:03 -0500

    Fineshed models for Groups and Posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth import get_user_model
+import misaka
+from goups.models import Group
+from django.conf import Settings
+
 
 # Create your models here.
